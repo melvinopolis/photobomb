0a2581054e3be52b97c4b978a95061aa8389c435
Mariel Van Norman <melvinopolis@gmail.com>
2015-08-27 15:28:02 -0400

    adds search function and styling with materialize

--- photobomb.rb
+++ photobomb.rb
@@ -7,7 +7,7 @@ get '/' do
 	erb :all_photos
 end
 
-def recent_photos(hashtag = "catsofinstagram", resolution = "thumbnail")
+def recent_photos(hashtag = "catsofinstagram", resolution = "low_resolution")
 	@photos = []
 	instagram_response = "https://api.instagram.com/v1/tags/#{hashtag}/media/recent?client_id=521aacb6282843c6ab4d890fd6c36049"
 	response = Unirest.get(instagram_response)
@@ -17,12 +17,19 @@ def recent_photos(hashtag = "catsofinstagram", resolution = "thumbnail")
 		image = photo["images"][resolution]["url"]
 		@photos << image
 	end
-
 	@photos
 end
+
+
 
 get '/:resolution/:hashtag' do
 	@title = "Photos"
 	@photos = recent_photos(params[:hashtag], params[:resolution])
+	erb :all_photos
+end
+
+post '/search' do
+	@title = params["hashtag"].capitalize
+	@photos = recent_photos(params["hashtag"], params["resolution"])
 	erb :all_photos
 end
